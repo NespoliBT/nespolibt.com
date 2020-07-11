f6ada318ca5f0705ef92c59444ca5f1d06c77cf1
Nespoli <nespoli.bt@gmail.com>
2020-07-11 18:57:16 +0200

    :construction: Edit Contacts page and highlight color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./css/Main.min.css";
-import Index from "./views/Index";
-import Progetti from "./views/Progetti";
 import Blog from "./views/Blog";
+import Index from "./views/Index";
 import Nav from "./views/Nav";
+import Contacts from "./views/Contacts";
+import Progetti from "./views/Progetti";
 
 function App() {
   let mobileMQ = window.matchMedia("(max-width: 800px)");
   const bg = require("./assets/sfondo.png");
   let appStyle = {
-    background: "#0e0e0e"
+    background: "#0e0e0e",
   };
 
   if (!mobileMQ.matches) {
     appStyle = {
       height: "100%",
       backgroundImage: `url(${bg})`,
-      backgroundSize: "cover"
+      backgroundSize: "cover",
     };
   }
   return (
@@ -28,6 +29,7 @@ function App() {
           <Route path="/" exact component={Index} />
           <Route path="/progetti" exact component={Progetti} />
           <Route path="/blog" exact component={Blog} />
+          <Route path="/contacts" exact component={Contacts} />
         </Switch>
       </Router>
     </div>
---
+++ src/views/Contacts.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../css/Contacts.min.css";
+
+function Contacts() {
+  return (
+    <div className="contacts">
+      <p>
+        Telegram:
+        <div className="contactContainer">
+          <a target="_blank" href="https://t.me/nespoli">
+            @Nespoli
+          </a>
+        </div>
+      </p>
+    </div>
+  );
+}
+
+export default Contacts;
