3eec717b208e5e6bdab4999e64da8d7a7a40e6c0
Nespoli <nespoli.bt@gmail.com>
2020-11-03 19:54:25 +0100

    :pencil2: typo

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -28,7 +28,7 @@ function Projects() {
         />
         <Project
           title="Note"
-          description="This was created becouse I was using TrackerBT to take notes and that's not how it's supposed to be used."
+          description="This was created because I was using TrackerBT to take notes and that's not how it's supposed to be used."
           version="1.0.0"
           techs={["html", "sass", "electron"]}
           repository="https://github.com/NespoliBT/Note"
